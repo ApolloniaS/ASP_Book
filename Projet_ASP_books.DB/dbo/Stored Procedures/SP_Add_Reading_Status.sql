ec6776d64a9b749513c2d49536958faa4978aa34
Apollonia Sorella <a.sorella@interface3.be>
2021-03-30 21:58:40 +0200

    up DB

--- Projet_ASP_books.DB/dbo/Stored Procedures/SP_Add_Reading_Status.sql
+++ Projet_ASP_books.DB/dbo/Stored Procedures/SP_Add_Reading_Status.sql
@@ -1,24 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[SP_Add_Reading_Status]
 	
 	@idBook INT,
-	@idUser INT,
-	@readingstatus BIT
+	@idUser INT
 	
 AS
-DECLARE @doExist BIT
-IF EXISTS( 
-	SELECT [idUserBook]
+SELECT [idUserBook]
       ,[idUser]
       ,[idBook]
-      ,[readingStatus]
+      ,CASE WHEN dbo.UserBook.readingStatus = 0 THEN 'À lire' WHEN dbo.UserBook.readingStatus = 1 THEN 'Lu' ELSE 'En cours de lecture' END AS readingStatus
     FROM [dbo].[UserBook]
 	WHERE dbo.UserBook.idBook = @idBook AND dbo.UserBook.idUser = @idUser
-)
-
-	SET @doExist = 1
-
-ELSE
-
-	SET @doExist = 0
-
-RETURN @doExist
